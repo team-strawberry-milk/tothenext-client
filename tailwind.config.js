a798acbee9ce3eca48407ff170a649cfb639af42
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-07-05 08:34:40 +0900

    style(#2): 메인 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        "primary-yello": "#FFC42C",
+        "primary-blue": "#305AD9",
+        "second-blue": "#4B58A5",
+        "third-blue": "#182859",
+        "primary-brown": "#260F01",
+      },
+    },
+  },
   plugins: [],
 };
