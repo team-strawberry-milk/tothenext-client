8c8c572343af2ed79891cd4ffd717fc28639b37c
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-07-09 05:42:59 +0900

    ui(#3): TextInput 컴포넌트 제작

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,3 +1,38 @@
+"use client";
+
+import TextInput from "@/components/auth/TextInput";
+import { useState } from "react";
+
 export default function SignIn() {
-  return <div className="">signin</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <section className="py-28 overflow-hidden">
+      <div className="max-w-lg mx-auto py-12 px-16 bg-white border border-slate-200 rounded-xl">
+        <h3 className="mb-5 text-3xl text-gray-500 font-semibold">Sign In</h3>
+        <p className="mb-8 text-base text-gray-300 font-medium">
+          Welcome back! Log in to continue.
+        </p>
+        <form action="#">
+          <div className="flex flex-wrap -m-3">
+            <TextInput
+              label="Email"
+              type="text"
+              placeholder="Email"
+              value={email}
+              setValue={(e) => setEmail(e.target.value)}
+            />
+            <TextInput
+              label="Password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              setValue={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
---
+++ components/auth/TextInput.js
@@ -0,0 +1,22 @@
+export default function TextInput({
+  label,
+  type,
+  placeholder,
+  value,
+  setValue,
+}) {
+  return (
+    <div className="w-full p-3">
+      <label className="mb-2 inline-block text-xs text-gray-300 font-medium">
+        {label}
+      </label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={setValue}
+        className="flex px-4 py-2.5 w-full font-medium bg-white hover:bg-gray-50 disabled:bg-gray-100 placeholder-gray-500 disabled:placeholder-gray-300 outline-none hover:placeholder-gray-600 active:ring-1 active:ring-lightgrey-600 active:ring-opacity-40 border border-gray-200 disabled:border-none rounded-md shadow-shadow-gray-100 active:shadow-none disabled:shadow-none disabled:pointer-events-none transition duration-200"
+      ></input>
+    </div>
+  );
+}
