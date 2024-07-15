cdafde17a1ec1fe665a90ed19d1eef759489577b
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-07-15 10:21:31 +0900

    fix(#17): axios 공통 변수 변수명 변경

--- api/http.js
+++ api/http.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
 
-const instance = axios.create({
+const http = axios.create({
   baseURL: `(나중에 정해질 서버 주소)/api`,
   headers: {
     "Content-Type": "application/json",
@@ -8,17 +8,18 @@ const instance = axios.create({
   withCredentials: true,
 });
 
-instance.interceptors.request.use(
+http.interceptors.request.use(
   (config) => {
-
-    config.headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, DELETE, OPTIONS';
-    config.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+    config.headers["Access-Control-Allow-Methods"] =
+      "GET, POST, PATCH, DELETE, OPTIONS";
+    config.headers["Access-Control-Allow-Headers"] =
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization";
 
     return config;
   },
   (error) => {
     return Promise.reject(error);
   }
-)
+);
 
-export default instance;
+export default http;
