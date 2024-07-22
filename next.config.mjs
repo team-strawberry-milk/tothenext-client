3db062f613dbd53f272989d9115d34056829bd1c
gaeun0nueag <87455786+gaeun0nueag@users.noreply.github.com>
2024-07-22 21:16:05 +0900

    feat: 로그인 api 연결

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
     env: {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     },
 };
 
